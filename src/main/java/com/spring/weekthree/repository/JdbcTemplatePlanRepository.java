3745223b77b76676106c17b7744b5e57f1d06ed4
backswan0 <rnrwprh3434@gmail.com>
2024-12-07 18:18:31 +0900

    :wrench: refactor/jh-main-repository
    declare final JDBC template as a class member and modify constructor parameter to use data source

--- src/main/java/com/spring/weekthree/repository/JdbcTemplatePlanRepository.java
+++ src/main/java/com/spring/weekthree/repository/JdbcTemplatePlanRepository.java
@@ -2,11 +2,21 @@ package com.spring.weekthree.repository;
 
 import com.spring.weekthree.dto.PlanResponseDto;
 import com.spring.weekthree.entity.Plan;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.time.LocalDate;
 import java.util.List;
 
+// @Repository 어노테이션을 절대 잊지 말자!!!
+@Repository
 public class JdbcTemplatePlanRepository implements PlanRepository{
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplatePlanRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
     @Override
     public PlanResponseDto save(Plan plan) {
