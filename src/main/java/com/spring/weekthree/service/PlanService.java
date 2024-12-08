a80329dda1264abf358978532ad04b6e510a4518
backswan0 <rnrwprh3434@gmail.com>
2024-12-08 14:50:42 +0900

    :broom: cleanup/jh-main-3 layers
    remove Java doc comments added for code change tracking

--- src/main/java/com/spring/weekthree/service/PlanService.java
+++ src/main/java/com/spring/weekthree/service/PlanService.java
@@ -6,19 +6,6 @@ import com.spring.weekthree.dto.PlanResponseDto;
 import java.time.LocalDate;
 import java.util.List;
 
-/**
- * Create 완료
- * Read 리팩토링 1차 완료 (if문 대신 스트림 사용, 목록 조회)
- * Read 완료 (단건 조회)
- * Update 완료 (PATCH)
- * Delete 완료
- * JDBC - Create 리팩토링 완료
- * JDBC - Read 리팩토링 중 (목록 조회)
- * JDBC - Read 리팩토링 완료 (예외처리 추가 수정, 단건 조회)
- * JDBC - Update 리팩토링 3차 완료 (클린 업 완료, 수정 날짜 바뀌도록 수정, 일부가 null일 때 예외 처리 전)
- * JDBC - Delete 리팩토링 완료
- */
-
 public interface PlanService {
     PlanResponseDto processSave(PlanRequestDto requestDto);
 
